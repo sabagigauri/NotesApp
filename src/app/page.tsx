551eb46e0b01f227059f6b303bd98ff0012d7421
Saba Gigauri <saba.gigauri@gmail.com>
2024-10-02 00:29:46 +0400

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './Components/header'
+import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Button from './Components/Button';
 
